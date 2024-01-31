09378fc5afd6374076941907c5f366ce7aebf5e1
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-01-31 18:19:47 +0000

    fractol : getting image from mlx

--- src/main.c
+++ src/main.c
@@ -5,10 +5,15 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 	t_display	display;
+	t_img		img;
 
 	display.mlx_conn = mlx_init();
 	display.mlx_win = mlx_new_window(display.mlx_conn, 
 						WIDTH, HEIGHT, "Fractol");
+
+	img.img = mlx_new_image(display.mlx_conn, WIDTH, HEIGHT);
+	img.pix = mlx_get_data_addr(img.img, 
+						&img.bpp, &img.line_len, &img.endian);
 
 	mlx_loop(display.mlx_conn);
 	return (0);
