2a619b68e72aa0078cd65e109dde87c8b1f149ad
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-02 16:52:13 +0000

    ft_math : cleaned

--- src/ft_math.c
+++ src/ft_math.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:42:35 by passunca          #+#    #+#             */
-/*   Updated: 2024/02/02 16:47:52 by passunca         ###   ########.fr       */
+/*   Updated: 2024/02/02 16:51:50 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ double	ft_scale(double unscaled_n, double new_min, double new_max,
 				double old_min, double old_max)
 {
 	return ((new_max - new_min) * (unscaled_n - old_min) 
-								/ (old_max - old_min) + new_min);
+				/ (old_max - old_min) + new_min);
 }
