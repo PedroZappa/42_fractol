3c220341d50fac3e338a7dfe1745753766010d06
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-02 17:43:53 +0000

    ft_math : added complex math operations

--- src/ft_math.c
+++ src/ft_math.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:42:35 by passunca          #+#    #+#             */
-/*   Updated: 2024/02/02 17:26:52 by passunca         ###   ########.fr       */
+/*   Updated: 2024/02/02 17:43:23 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,16 @@ double	ft_scale(double unscaled_n, double max,
  *	*/
 t_complex	ft_complex_sum(t_complex c1, t_complex c2)
 {
-	return ((t_complex){c1.r + c2.r, c1.i + c2.i});
+	return ((t_complex){(c1.r + c2.r), (c1.i + c2.i)});
+}
+
+/*	Squaring a complex number 
+ *		(a + bi)^2 = (a² - b²) + 2abi
+ *
+ *		c.r = ((c.r * c.r) - (c.i * c.i));
+ *		c.i = (2 * a * bi);
+* */
+t_complex	ft_complex_square(t_complex c)
+{
+	return ((t_complex){((c.r * c.r) - (c.i * c.i)), (2 * c.r * c.i)});
 }
