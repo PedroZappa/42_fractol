b60bdb89fa981ca972e7f6127329c02da84414a9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-09 16:25:53 +0000

    ft_events : renamed display to d

--- src/ft_events.c
+++ src/ft_events.c
@@ -16,13 +16,13 @@
 *	Setup hook for mouse clicks;
 *	Setup hook to listen clicking X on the window
  *	*/
-void	ft_events_init(t_display *display)
+void	ft_events_init(t_display *d)
 {
-	mlx_hook(display->mlx_win, KeyPress, KeyPressMask, ft_handle_keys, display);
-	mlx_hook(display->mlx_win, DestroyNotify, StructureNotifyMask, 
-		  ft_kill_handle, display);
-	mlx_hook(display->mlx_win, ButtonPress, ButtonPressMask, ft_handle_mouse, 
-		  display);
+	mlx_hook(d->mlx_win, KeyPress, KeyPressMask, ft_handle_keys, d);
+	mlx_hook(d->mlx_win, DestroyNotify, StructureNotifyMask, 
+		  ft_kill_handle, d);
+	mlx_hook(d->mlx_win, ButtonPress, ButtonPressMask, ft_handle_mouse, 
+		  d);
 }
 
 /*	Handle key input: int (*f)(int keycode, void *param)
@@ -31,25 +31,25 @@ void	ft_events_init(t_display *display)
  *		Handle Up and down offset, scale it with zoom
  *		Handle change of iterations (PageUp PageDown);
  *	*/
-int	ft_handle_keys(int keysym, t_display *display)
+int	ft_handle_keys(int keysym, t_display *d)
 {
 	if (keysym == XK_Escape)
-		ft_kill_handle(display);
+		ft_kill_handle(d);
 	else if (keysym == XK_Left || keysym == XK_KP_Left)
-		display->x_offset -= (0.3 * display->zoom);
+		d->x_offset -= (0.3 * d->zoom);
 	else if (keysym == XK_Right || keysym == XK_KP_Right)
-		display->x_offset += (0.3 * display->zoom);
+		d->x_offset += (0.3 * d->zoom);
 	else if (keysym == XK_Up || keysym == XK_KP_Up)
-		display->y_offset -= (0.3 * display->zoom);
+		d->y_offset -= (0.3 * d->zoom);
 	else if (keysym == XK_Down || keysym == XK_KP_Down)
-		display->y_offset += (0.3 * display->zoom);
+		d->y_offset += (0.3 * d->zoom);
 	else if (keysym == XK_Page_Up)
-		display->iter += 21;
+		d->iter += 21;
 	else if (keysym == XK_Page_Down)
-		display->iter -= 21;
+		d->iter -= 21;
 	else
 		ft_printf("Unknown key: %d\n", keysym);
-	ft_render(display);
+	ft_render(d);
 	return (0);
 }
 
@@ -58,17 +58,17 @@ int	ft_handle_keys(int keysym, t_display *display)
  *		Handle Zoom out with mouse wheel
  *
  *	*/
-int	ft_handle_mouse(int button, int x, int y, t_display *display)
+int	ft_handle_mouse(int button, int x, int y, t_display *d)
 {
 	(void)x;
 	(void)y;
 	(void)button;
 	if (button == Button4)
-		display->zoom *= 0.95;
+		d->zoom *= 0.95;
 	else if (button == Button5)
-		display->zoom *= 1.05;
+		d->zoom *= 1.05;
 	else
 		ft_printf("Unknown mouse: %d\n", button);
-	ft_render(display);
+	ft_render(d);
 	return (0);
 }
