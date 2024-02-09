4c14a8ccb071ba96d2bf37d724adbfb9518910cb
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-09 11:08:18 +0000

    ft_help.c : Help message improved

--- src/ft_help.c
+++ src/ft_help.c
@@ -14,25 +14,26 @@
 
 int	ft_usage(void)
 {
+	ft_sep_color('#', '=', 30, GRN);
 	ft_pout_color("Fract'ol Usage:\n", YEL);
-	ft_printf("\t./fractol <set> <iterations> <c.r> <c.i>\n");
+	ft_printf("\t%s./fractol <set> <iterations> <c.r> <c.i>%s\n",
+		CYN, NC);
+	ft_sep_color('#', '=', 50, GRN);
+	ft_pout_color("Available Sets:\n", YEL);
+	ft_printf("> Mandelbrot\n\t%s./fractol <set>%s\n",
+		CYN, NC);
+	ft_printf("> Julia\n\t%s./fractol <set> <iterations> <c.r> <c.i>%s\n",
+		CYN, NC);
+	ft_printf("> Newton\n\t%s./fractol <set> <iterations> <c.r> <c.i>%s\n",
+		CYN, NC);
 	ft_sep_color('#', '=', 50, GRN);
 	ft_pout_color("Controls:\n", YEL);
-	ft_printf(":Move around with:\n");
-	ft_printf("\t> Arrow keys\n\t> Numpad keys\n");
-	ft_printf(":Zoom in and out with:\n");
-	ft_printf("\t> Mouse wheel\n");
-	ft_printf(":Change n iterations with:\n");
-	ft_printf("\t> PageUp & PageDown\n");
-	ft_printf(":Change color scheme with:\n");
-	ft_printf("\t> Spacebar\n");
-	ft_printf(":Switch between Sets with:\n");
-	ft_printf("\t> 1, 2, 3\n");
-	ft_printf(":To Exit press:\n");
-	ft_printf("\t> ESC\n\t> Window's X button\n");
-	ft_sep_color('#', '=', 30, GRN);
-	ft_pout_color("Available Sets:\n", YEL);
-	ft_printf("\t> Mandelbrot\n\t> Julia\n\t> Newton\n");
+	ft_printf(":Move around with:\n\t> Arrow keys\n\t> Numpad keys\n");
+	ft_printf(":Zoom in and out with:\n\t> Mouse wheel\n");
+	ft_printf(":Change n iterations with:\n\t> PageUp & PageDown\n");
+	ft_printf(":Change color scheme with:\n\t> Spacebar\n");
+	ft_printf(":Switch between Sets with:\n\t> 1, 2, 3\n");
+	ft_printf(":To Exit press:\n\t> ESC\n\t> Window's X button\n");
 	ft_sep_color('#', '=', 30, GRN);
 	return (0);
 }
