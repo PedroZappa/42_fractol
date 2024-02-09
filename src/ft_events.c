7bcb6bdaedfc194c0ef5426a2a0905434ad64559
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-09 16:21:39 +0000

    Key hooks fixed

--- src/ft_events.c
+++ src/ft_events.c
@@ -21,9 +21,8 @@ void	ft_events_init(t_display *display)
 	mlx_hook(display->mlx_win, KeyPress, KeyPressMask, ft_handle_keys, display);
 	mlx_hook(display->mlx_win, DestroyNotify, StructureNotifyMask, 
 		  ft_kill_handle, display);
-	mlx_hook(display->mlx_win, ButtonPress, ButtonPressMask, ft_zoom, display);
-	// mlx_hook(display->mlx_win, ButtonPress, ButtonPressMask, ft_handle_mouse, 
-	// 	  display);
+	mlx_hook(display->mlx_win, ButtonPress, ButtonPressMask, ft_handle_mouse, 
+		  display);
 }
 
 /*	Handle key input: int (*f)(int keycode, void *param)
@@ -37,9 +36,9 @@ int	ft_handle_keys(int keysym, t_display *display)
 	if (keysym == XK_Escape)
 		ft_kill_handle(display);
 	else if (keysym == XK_Left || keysym == XK_KP_Left)
-		display->x_offset += (0.3 * display->zoom);
-	else if (keysym == XK_Right || keysym == XK_KP_Right)
 		display->x_offset -= (0.3 * display->zoom);
+	else if (keysym == XK_Right || keysym == XK_KP_Right)
+		display->x_offset += (0.3 * display->zoom);
 	else if (keysym == XK_Up || keysym == XK_KP_Up)
 		display->y_offset -= (0.3 * display->zoom);
 	else if (keysym == XK_Down || keysym == XK_KP_Down)
@@ -64,12 +63,12 @@ int	ft_handle_mouse(int button, int x, int y, t_display *display)
 	(void)x;
 	(void)y;
 	(void)button;
-	// if (button == Button4)
-	// 	display->zoom *= 0.95;
-	// else if (button == Button5)
-	// 	display->zoom *= 1.05;
-	// else
-	// 	ft_printf("Unknown mouse: %d\n", button);
+	if (button == Button4)
+		display->zoom *= 0.95;
+	else if (button == Button5)
+		display->zoom *= 1.05;
+	else
+		ft_printf("Unknown mouse: %d\n", button);
 	ft_render(display);
 	return (0);
 }
