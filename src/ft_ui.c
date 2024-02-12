4ac3d09243225123a4120c946b7d81977e3d6520
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-12 16:33:11 +0000

    ft_ui.c : Basic UI implemented

--- src/ft_ui.c
+++ src/ft_ui.c
@@ -6,26 +6,48 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 16:05:07 by passunca          #+#    #+#             */
-/*   Updated: 2024/02/12 16:08:24 by passunca         ###   ########.fr       */
+/*   Updated: 2024/02/12 16:25:55 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+static void ft_mandelbrot_ui(t_display *d);
+static void ft_julia_ui(t_display *d);
+
 void	ft_render_ui(t_display *d)
 {
 	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 20, HEX_WHITE, d->name);
 	if (!ft_strcmp(d->name, "Mandelbrot"))
-	{
-		mlx_string_put(d->mlx_conn, d->mlx_win, 15, 40, HEX_WHITE, 
-				 ft_strjoin("Zoom : ", ft_dtoa(d->zoom)));
-		mlx_string_put(d->mlx_conn, d->mlx_win, 15, 60, HEX_WHITE, ft_dtoa(d->c.r));
-		mlx_string_put(d->mlx_conn, d->mlx_win, 15, 80, HEX_WHITE, ft_dtoa(d->c.i));
-	}
+		ft_mandelbrot_ui(d);
 	else if (!ft_strcmp(d->name, "Julia"))
-	{
-		mlx_string_put(d->mlx_conn, d->mlx_win, 15, 40, HEX_WHITE, ft_dtoa(d->zoom));
-		mlx_string_put(d->mlx_conn, d->mlx_win, 15, 60, HEX_WHITE, ft_dtoa(d->c_julia.r));
-		mlx_string_put(d->mlx_conn, d->mlx_win, 15, 80, HEX_WHITE, ft_dtoa(d->c_julia.i));
-	}
+		ft_julia_ui(d);
+}
+
+static void ft_mandelbrot_ui(t_display *d)
+{
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 40, HEX_WHITE,
+		ft_strjoin("Zoom : ", ft_dtoa(d->zoom)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 60, HEX_WHITE,
+		ft_strjoin("z.r = ", ft_dtoa(d->z.r)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 80, HEX_WHITE,
+		ft_strjoin("z.i = ", ft_dtoa(d->z.i)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 100, HEX_WHITE,
+		ft_strjoin("c.r = ", ft_dtoa(d->c.r)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 120, HEX_WHITE,
+		ft_strjoin("c.i = ", ft_dtoa(d->c.i)));
+}
+
+static void ft_julia_ui(t_display *d)
+{
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 40, HEX_WHITE,
+		ft_strjoin("Zoom = ", ft_dtoa(d->zoom)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 60, HEX_WHITE,
+		ft_strjoin("z.r = ", ft_dtoa(d->z.r)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 80, HEX_WHITE,
+		ft_strjoin("z.i = ", ft_dtoa(d->z.i)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 100, HEX_WHITE,
+		ft_strjoin("c.r = ", ft_dtoa(d->c_julia.r)));
+	mlx_string_put(d->mlx_conn, d->mlx_win, 15, 120, HEX_WHITE,
+		ft_strjoin("c.i = ", ft_dtoa(d->c_julia.i)));
 }
