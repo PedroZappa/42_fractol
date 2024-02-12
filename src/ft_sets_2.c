79941df29fcd88618f3c44052f1a12490d46e6cf
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-12 22:39:01 +0000

    ft_sets_2.c : cleanup

--- src/ft_sets_2.c
+++ src/ft_sets_2.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 21:34:26 by passunca          #+#    #+#             */
-/*   Updated: 2024/02/12 22:19:31 by passunca         ###   ########.fr       */
+/*   Updated: 2024/02/12 22:38:34 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void			calculate_newton(t_display *d);
 static unsigned		ft_check_roots(t_display *d);
 static void			ft_init_roots(t_complex *roots);
-static t_complex	ft_get_diff(t_complex z, t_complex root);
+static t_complex	ft_get_diff(t_complex z_newton, t_complex root);
 
 /*
  *	f(n)(c) = f(n-1)(c)² + c
@@ -46,17 +46,17 @@ static void	calculate_newton(t_display *d)
 	t_complex	next;
 	double		denominator;
 
-	denominator = (3 * ((d->z.r * d->z.r) + (d->z.i * d->z.i)));
-	next.r = (((d->z.r * d->z.r * d->z.r * d->z.r * d->z.r)
-		+ (2 * d->z.r * d->z.r * d->z.r * d->z.i * d->z.i) - (d->z.r * d->z.r)
-		+ (d->z.r * d->z.i * d->z.i * d->z.i * d->z.i) + (d->z.i * d->z.i))
+	denominator = (3 * ((d->z_newton.r * d->z_newton.r) + (d->z_newton.i * d->z_newton.i)));
+	next.r = (((d->z_newton.r * d->z_newton.r * d->z_newton.r * d->z_newton.r * d->z_newton.r)
+		+ (2 * d->z_newton.r * d->z_newton.r * d->z_newton.r * d->z_newton.i * d->z_newton.i) - (d->z_newton.r * d->z_newton.r)
+		+ (d->z_newton.r * d->z_newton.i * d->z_newton.i * d->z_newton.i * d->z_newton.i) + (d->z_newton.i * d->z_newton.i))
 			/ denominator);
-	next.i = ((d->z.i * ((d->z.r * d->z.r * d->z.r * d->z.r)
-		+ (2 * d->z.r * d->z.r * d->z.i * d->z.i) + (2 * d->z.r)
-		+ (d->z.i * d->z.i * d->z.i * d->z.i))
+	next.i = ((d->z_newton.i * ((d->z_newton.r * d->z_newton.r * d->z_newton.r * d->z_newton.r)
+		+ (2 * d->z_newton.r * d->z_newton.r * d->z_newton.i * d->z_newton.i) + (2 * d->z_newton.r)
+		+ (d->z_newton.i * d->z_newton.i * d->z_newton.i * d->z_newton.i))
 			/ denominator));
-	d->z.r -= next.r;
-	d->z.i -= next.i;
+	d->z_newton.r -= next.r;
+	d->z_newton.i -= next.i;
 }
 
 static unsigned ft_check_roots(t_display *d)
@@ -65,15 +65,15 @@ static unsigned ft_check_roots(t_display *d)
 	t_complex			diff;
 
 	ft_init_roots(roots);
-	diff = ft_get_diff(d->z, roots[0]);
+	diff = ft_get_diff(d->z_newton, roots[0]);
 	if ((ft_abs(diff.r) < d->newton_escape)
 		&& (ft_abs(diff.i) < d->newton_escape))
 		return (1);
-	diff = ft_get_diff(d->z, roots[1]);
+	diff = ft_get_diff(d->z_newton, roots[1]);
 	if ((ft_abs(diff.r) < d->newton_escape)
 		&& (ft_abs(diff.i) < d->newton_escape))
 		return (1);
-	diff = ft_get_diff(d->z, roots[2]);
+	diff = ft_get_diff(d->z_newton, roots[2]);
 	if ((ft_abs(diff.r) < d->newton_escape)
 		&& (ft_abs(diff.i) < d->newton_escape))
 		return (1);
@@ -93,11 +93,11 @@ static void	ft_init_roots(t_complex *roots)
 	}
 }
 
-static t_complex	ft_get_diff(t_complex z, t_complex root)
+static t_complex	ft_get_diff(t_complex z_newton, t_complex root)
 {
 	t_complex	diff;
 
-	diff.r = (z.r - root.r);
-	diff.i = (z.i - root.i);
+	diff.r = (z_newton.r - root.r);
+	diff.i = (z_newton.i - root.i);
 	return (diff);
 }
