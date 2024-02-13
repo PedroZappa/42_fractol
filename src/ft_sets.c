3c2102e9a6ddc66d78eeaf5e9806a623c5306daf
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-13 21:31:03 +0000

    ft_sets.c : Implemented Tricorn

--- src/ft_sets.c
+++ src/ft_sets.c
@@ -93,25 +93,4 @@ void	render_tricorn(t_display *d, int x, int y)
 		}
 	}
 	ft_put_pixel(d->img, x, y, d->color);
-	// t_complex	z_tmp;
-	// int			color;
-	// int			i;
-	//
-	// i = 0;
-	// d->z.r = 0;
-	// d->z.i = 0;
-	// while (++i <= d->iter)
-	// {
-	// 	z_tmp.r = d->z.r;
-	// 	z_tmp.i = d->z.i;
-	// 	d->z.r = ((z_tmp.r * z_tmp.r) - (z_tmp.i * z_tmp.i) + d->c.r);
-	// 	d->z.i = ((-2 * z_tmp.r * z_tmp.i) + d->c.i);
-	// 	if (((d->z.r * d->z.r) + (d->z.i * d->z.i)) > d->escape)
-	// 	{
-	// 		color = (ft_map(i, d->color_iter, d->color_range) * 5);
-	// 		ft_put_pixel(d->img, x, y, color);
-	// 		return ;
-	// 	}
-	// }
-	// ft_put_pixel(d->img, x, y, d->color);
 }
