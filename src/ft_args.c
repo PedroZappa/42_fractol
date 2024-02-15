ee8b99a4cf01d34f10bfce4c3db0c4f377d8ae1f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-15 11:13:02 +0000

    ft_help.c : Help message improved

--- src/ft_args.c
+++ src/ft_args.c
@@ -12,70 +12,62 @@
 
 #include "fractol.h"
 
-static int	ft_set_args(t_display *display, int argc, char **argv);
+static int	ft_set_args(t_display *d, int argc, char **argv);
 static int	ft_invalid_args(char *name);
+static void	ft_set_fractal(t_display *d, int set, char *name, int iter);
 
 /*	Command line arguments checker
  *	*/
-int	ft_args(t_display *display, int argc, char **argv)
+int	ft_args(t_display *d, int argc, char **argv)
 {
-	if (!ft_select_fractal(display, argc, argv))
+	if (!ft_select_fractal(d, argc, argv))
 		return (ft_invalid_args(argv[1]));
-	if (!ft_set_args(display, argc, argv))
+	if (!ft_set_args(d, argc, argv))
 		return (0);
 	return (1);
 }
 
-int	ft_select_fractal(t_display *display, int argc, char **argv)
+int	ft_select_fractal(t_display *d, int argc, char **argv)
 {
 	char	*name;
 
 	name = ft_stolower(argv[1]);
 	if (!ft_strcmp(name, "mandelbrot") && (argc == 3))
-	{
-		display->set = MANDELBROT;
-		display->name = "Mandelbrot";
-		display->iter = ft_atoi(argv[2]);
-	}
+		ft_set_fractal(d, MANDELBROT, "Mandelbrot", ft_atoi(argv[2]));
 	else if (!ft_strcmp(name, "julia") && ((argc == 3) || (argc == 5)))
-	{
-		display->set = JULIA;
-		display->name = "Julia";
-		display->iter = ft_atoi(argv[2]);
-	}
+		ft_set_fractal(d, JULIA, "Julia", ft_atoi(argv[2]));
 	else if (!ft_strcmp(name, "tricorn") && (argc == 3))
-	{
-		display->set = TRICORN;
-		display->name = "Tricorn";
-		display->iter = ft_atoi(argv[2]);
-	}
+		ft_set_fractal(d, TRICORN, "Tricorn", ft_atoi(argv[2]));
 	else if (!ft_strcmp(name, "newton") && (argc == 3))
-	{
-		display->set = NEWTON;
-		display->name = "Newton";
-		display->iter = ft_atoi(argv[2]);
-	}
+		ft_set_fractal(d, NEWTON, "Newton", ft_atoi(argv[2]));
 	else
 		return (0);
 	return (1);
 }
 
-static int	ft_set_args(t_display *display, int argc, char **argv)
+static void	ft_set_fractal(t_display *d, int set, char *name, int iter)
+{
+	d->set = set;
+	d->name = name;
+	d->iter = iter;
+}
+
+static int	ft_set_args(t_display *d, int argc, char **argv)
 {
 	if (((argc > 2) && (argc < 5)) && !ft_is_argint(argv[2]))
 		return (ft_kill_werror("Invalid arguments\n"));
 	else if ((argc == 3) || (argc == 5))
 	{
-		display->iter = ft_atoi(argv[2]);
-		if (display->iter <= 0)
-			display->iter = INIT_ITER;
+		d->iter = ft_atoi(argv[2]);
+		if (d->iter <= 0)
+			d->iter = INIT_ITER;
 	}
 	if ((argc > 5) && ((!ft_is_argdbl(argv[3])) || (!ft_is_argdbl(argv[4]))))
 		return (ft_kill_werror("Invalid argument type\n"));
 	else if (argc == 5)
 	{
-		display->c_julia.r = ft_atod(argv[3]);
-		display->c_julia.i = ft_atod(argv[4]);
+		d->c_julia.r = ft_atod(argv[3]);
+		d->c_julia.i = ft_atod(argv[4]);
 	}
 	return (1);
 }
--- src/ft_help.c
+++ src/ft_help.c
@@ -5,33 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/02 11:50:18 by passunca          #+#    #+#             */
+/*   Created: 2024/02/02 11:45:18 by passunca          #+#    #+#             */
 /*   Updated: 2024/02/12 17:05:27 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+static void	ft_controls(void);
+
 int	ft_usage(void)
 {
-	ft_sep_color('#', '=', 50, GRN);
-	ft_pout_color("Fract'ol Usage:\n", YEL);
-	ft_printf("\t%s./fractol <set> <iter> <c.r> <c.i>%s\n", CYN, NC);
-	ft_sep_color('#', '=', 50, GRN);
-	ft_pout_color("Available Sets:\n", YEL);
-	ft_printf("> Mandelbrot\n\t%s./fractol <set>%s\n", CYN, NC);
-	ft_printf("> Julia\n\t%s./fractol <set> <iter> <c.r> <c.i>%s\n", CYN, NC);
-	ft_printf("> Newton\n\t...\n");
-	ft_sep_color('#', '=', 50, GRN);
-	ft_pout_color("Controls:\n", YEL);
-	ft_printf(":Move around with:\n\t> Arrow keys\n\t> Numpad keys\n");
-	ft_printf(":Zoom in and out with:\n\t> Mouse wheel\n");
-	ft_printf(":Change n iterations with:\n\t> PageUp & PageDown\n");
-	ft_printf(":Change color scheme with:\n\t> L-Shift & R-Shift\n");
-	ft_printf(":Switch between Sets with:\n\t> Spacebar\n");
-	ft_printf(":To Exit press:\n\t> ESC\n\t> Window's X button\n");
-	ft_sep_color('#', '=', 50, GRN);
+	ft_sep_color('#', '=', 45, GRN);
+	ft_pout_color(">> Fract'ol Usage <<\n", MAG);
+	ft_sep_color('#', '=', 45, GRN);
+	ft_pout_color("\tAvailable Sets\n", YEL);
+	ft_sep_color('#', '=', 45, GRN);
+	ft_printf("> Mandelbrot\n\t%s./fractol mandelbrot <iter>%s\n", CYN, NC);
+	ft_printf("> Julia\n\t%s./fractol julia <iter> <c.r> <c.i>%s\n", CYN, NC);
+	ft_printf("> Tricorn\n\t%s./fractol tricorn <iter>%s\n", CYN, NC);
+	ft_printf("> Newton\n\t%s./fractol newton <iter>%s\n", CYN, NC);
+	ft_controls();
 	return (0);
+}
+
+static void	ft_controls(void)
+{
+	ft_sep_color('#', '=', 45, GRN);
+	ft_pout_color("\tControls\n", YEL);
+	ft_sep_color('#', '=', 45, GRN);
+	ft_printf(":Move around with:\n\t> %sArrow keys%s\n", CYN, NC);
+	ft_printf(":Zoom in and out with:\n\t> %sMouse wheel%s\n", CYN, NC);
+	ft_printf(":Change n iterations with:\n\t> %sPageUp & PageDown%s\n",
+		   CYN, NC);
+	ft_printf(":Change color scheme with:\n\t> %sL-Shift & R-Shift%s\n",
+		   CYN, NC);
+	ft_printf(":Toggle between Sets with:\n\t> %sSpacebar%s\n", CYN, NC);
+	ft_printf(":Select a specific Set with:\n\t", CYN, NC);
+	ft_printf("> %s1, 2, 3, 4%s\n", CYN, NC);
+	ft_printf(":To Exit press:\n\t> %sESC%s\n", CYN, NC);
+	ft_sep_color('#', '=', 45, GRN);
 }
 
 int	ft_no_args(void)
